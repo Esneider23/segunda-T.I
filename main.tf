f28f4f7edd0d9d021683ce4439b6d1f3e0540b39
Esneider23 <guzmane@utb.edu.co>
2023-10-27 14:37:47 -0500

    update

--- main.tf
+++ main.tf
@@ -16,10 +16,6 @@ variable "imagebuild" {
   description = "the latest image build version"
 }
 
-variable "app_count" {
-  type = number
-  default = 1
-}
 
 data "aws_availability_zones" "available_zones" {
   state = "available"
@@ -60,74 +56,104 @@ resource "aws_ecs_task_definition" "app_task" {
   execution_role_arn       = data.aws_iam_role.ecsTaskExecutionRole.arn 
 }
 
-resource "aws_vpc" "default" {
-    cidr_block = "10.32.0.0/16"
+resource "aws_vpc" "default_vpc" {
 }
 
-resource "aws_subnet" "public" {
-    count                   = 2
-    cidr_block              = cidrsubnet(aws_vpc.default.cidr_block, 8, 2 + count.index)
-    availability_zone       = data.aws_availability_zones.available_zones.names[count.index]
-    vpc_id                  = aws_vpc.default.id
-    map_public_ip_on_launch = true
+resource "aws_default_subnet" "default_subnet_a" {
+  # Use your own region here but reference to subnet 1a
+  availability_zone = "us-east-1a"
 }
 
-resource "aws_subnet" "private" {
-  count             = 2
-  cidr_block        = cidrsubnet(aws_vpc.default.cidr_block, 8, count.index)
-  availability_zone = data.aws_availability_zones.available_zones.names[count.index]
-  vpc_id            = aws_vpc.default.id
+resource "aws_default_subnet" "default_subnet_b" {
+  # Use your own region here but reference to subnet 1b
+  availability_zone = "us-east-1b"
 }
 
-resource "aws_security_group" "lb" {
-  name        = "example-alb-security-group"
-  vpc_id      = aws_vpc.default.id
 
+resource "aws_alb" "application_load_balancer" {
+  name               = "load-balancer-dev" #load balancer name
+  load_balancer_type = "application"
+  subnets = [ # Referencing the default subnets
+    "${aws_default_subnet.default_subnet_a.id}",
+    "${aws_default_subnet.default_subnet_b.id}"
+  ]
+  # security group
+  security_groups = ["${aws_security_group.load_balancer_security_group.id}"]
+}
+
+resource "aws_security_group" "load_balancer_security_group" {
   ingress {
-    protocol    = "tcp"
     from_port   = 80
     to_port     = 80
-    cidr_blocks = ["0.0.0.0/0"]
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"] # Allow traffic in from all sources
   }
 
   egress {
-    from_port = 0
-    to_port   = 0
-    protocol  = "-1"
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
 }
 
-resource "aws_lb" "default" {
-  name            = "example-lb"
-  load_balancer_type = "application"
-  subnets         = aws_subnet.public.*.id
-  security_groups = [aws_security_group.lb.id]
-}
 
 resource "aws_lb_target_group" "target_group" {
-  name        = "Segunda-TI"
+  name        = "target-group"
   port        = 80
   protocol    = "HTTP"
-  vpc_id      = aws_vpc.default.id
   target_type = "ip"
+  vpc_id      = "${aws_default_vpc.default_vpc.id}" # default VPC
 }
 
 resource "aws_lb_listener" "listener" {
-  load_balancer_arn = aws_lb.default.id
+  load_balancer_arn = "${aws_alb.application_load_balancer.arn}" #  load balancer
   port              = "80"
   protocol          = "HTTP"
-
   default_action {
-    target_group_arn = aws_lb_target_group.target_group.id
     type             = "forward"
+    target_group_arn = "${aws_lb_target_group.target_group.arn}" # target group
   }
 }
 
 
+resource "aws_ecs_service" "app_service" {
+  name            = "app-first-service"     # Name the service
+  cluster         = "${aws_ecs_cluster.my_cluster.id}"   # Reference the created Cluster
+  task_definition = "${aws_ecs_task_definition.app_task.arn}" # Reference the task that the service will spin up
+  launch_type     = "FARGATE"
+  desired_count   = 3 # Set up the number of containers to 3
 
+  load_balancer {
+    target_group_arn = "${aws_lb_target_group.target_group.arn}" # Reference the target group
+    container_name   = "${aws_ecs_task_definition.app_task.family}"
+    container_port   = 5000 # Specify the container port
+  }
 
+  network_configuration {
+    subnets          = ["${aws_default_subnet.default_subnet_a.id}", "${aws_default_subnet.default_subnet_b.id}"]
+    assign_public_ip = true     # Provide the containers with public IPs
+    security_groups  = ["${aws_security_group.service_security_group.id}"] # Set up the security group
+  }
+}
 
+resource "aws_security_group" "service_security_group" {
+  ingress {
+    from_port = 0
+    to_port   = 0
+    protocol  = "-1"
+    # Only allowing traffic in from the load balancer security group
+    security_groups = ["${aws_security_group.load_balancer_security_group.id}"]
+  }
 
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
 
-
+output "app_url" {
+  value = aws_alb.application_load_balancer.dns_name
+}
