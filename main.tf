c92106b294b1e7ee4433f3b02f2bcb1b19957f0f
Esneider23 <guzmane@utb.edu.co>
2023-10-27 14:03:25 -0500

    update

--- main.tf
+++ main.tf
@@ -30,11 +30,6 @@ data "aws_iam_role" "ecsTaskExecutionRole" {
 }
 
 
-resource "aws_iam_role_policy_attachment" "ecsTaskExecutionRole_policy" {
-  role       = "${aws_iam_role.ecsTaskExecutionRole.name}"
-  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
-}
-
 resource "aws_ecs_task_definition" "app_task" {
   family                   = "app-first-task" 
   container_definitions    = <<DEFINITION
