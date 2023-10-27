4bd18d4b1dc585b59c0cb4e965b76eb61ef4f6c9
Esneider23 <guzmane@utb.edu.co>
2023-10-27 13:56:41 -0500

    test

--- main.tf
+++ main.tf
@@ -20,36 +20,9 @@ data "aws_availability_zones" "available_zones" {
   state = "available"
 }
 
-# main.tf
+
 resource "aws_ecs_cluster" "my_cluster" {
   name = "ACTIVIDAD-TI" 
-}
-
-# main.tf
-resource "aws_ecs_task_definition" "app_task" {
-  family                   = "app-first-task"
-  container_definitions    = <<DEFINITION
-  [
-    {
-      "name": "app-first-task",
-      "image": "244410002174.dkr.ecr.us-east-1.amazonaws.com/t.i/segunda-actividad:${imagebuild}",
-      "essential": true,
-      "portMappings": [
-        {
-          "containerPort": 5000,
-          "hostPort": 5000
-        }
-      ],
-      "memory": 512,
-      "cpu": 256
-    }
-  ]
-  DEFINITION
-  requires_compatibilities = ["FARGATE"] # use Fargate as the launch type
-  network_mode             = "awsvpc"    # add the AWS VPN network mode as this is required for Fargate
-  memory                   = 512         # Specify the memory the container requires
-  cpu                      = 256         # Specify the CPU the container requires
-  execution_role_arn       = "${aws_iam_role.ecsTaskExecutionRole.arn}"
 }
 
 resource "aws_iam_role" "ecsTaskExecutionRole" {
@@ -72,3 +45,34 @@ resource "aws_iam_role_policy_attachment" "ecsTaskExecutionRole_policy" {
   role       = "${aws_iam_role.ecsTaskExecutionRole.name}"
   policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
 }
+
+resource "aws_ecs_task_definition" "app_task" {
+  family                   = "app-first-task" 
+  container_definitions    = <<DEFINITION
+  [
+    {
+      "name": "app-first-task",
+      "image": "244410002174.dkr.ecr.us-east-1.amazonaws.com/t.i/segunda-actividad:${var.imagebuild}",
+      "essential": true,
+      "portMappings": [
+        {
+          "containerPort": 80,
+          "hostPort": 80
+        }
+      ],
+      "memory": 512,
+      "cpu": 256
+    }
+  ]
+  DEFINITION
+  requires_compatibilities = ["FARGATE"] # use Fargate as the launch type
+  network_mode             = "awsvpc"    # add the AWS VPN network mode as this is required for Fargate
+  memory                   = 512         # Specify the memory the container requires
+  cpu                      = 256         # Specify the CPU the container requires
+  execution_role_arn       = aws_iam_role.ecsTaskExecutionRole.arn
+}
+
+
+
+
+
