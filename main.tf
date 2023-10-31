4545237438305895fc9b8eee4fd93f832d1579ed
Esneider23 <guzmane@utb.edu.co>
2023-10-31 08:59:08 -0500

    victor 2

--- main.tf
+++ main.tf
@@ -24,6 +24,10 @@ resource "aws_ecs_cluster" "utb_cluster" {
   name = "utbapp" # Nombre del cluster
 }
 
+data "aws_iam_role" "ecsTaskExecutionRole" {
+  name = "ecsTaskExecutionRole"
+}
+
 resource "aws_ecs_task_definition" "app_task" {
   family                   = "app-first-task" # Name your task
   container_definitions    = <<DEFINITION
@@ -47,29 +51,9 @@ resource "aws_ecs_task_definition" "app_task" {
   network_mode             = "awsvpc"    # add the AWS VPN network mode as this is required for Fargate
   memory                   = 512         # Specify the memory the container requires
   cpu                      = 256         # Specify the CPU the container requires
-  execution_role_arn       = "${aws_iam_role.ecsTaskExecutionRole.arn}"
+  execution_role_arn       = data.aws_iam_role.ecsTaskExecutionRole.arn
 }
 
-resource "aws_iam_role" "ecsTaskExecutionRole" {
-  name               = "ecsTaskExecutionRole"
-  assume_role_policy = "${data.aws_iam_policy_document.assume_role_policy.json}"
-}
-
-data "aws_iam_policy_document" "assume_role_policy" {
-  statement {
-    actions = ["sts:AssumeRole"]
-
-    principals {
-      type        = "Service"
-      identifiers = ["ecs-tasks.amazonaws.com"]
-    }
-  }
-}
-
-resource "aws_iam_role_policy_attachment" "ecsTaskExecutionRole_policy" {
-  role       = "${aws_iam_role.ecsTaskExecutionRole.name}"
-  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
-}
 
 # Provide a reference to your default VPC
 resource "aws_default_vpc" "default_vpc" {
